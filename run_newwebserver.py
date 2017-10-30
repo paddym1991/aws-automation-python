9f95ff2d3a21c852ab67f2be6503c9ff580859e5
paddym1991 <paddym1991.pm@gmail.com>
2017-10-30 22:41:49 +0000

    changes to add bucket and adding image to index/nginx

--- run_newwebserver.py
+++ run_newwebserver.py
@@ -5,6 +5,7 @@ import time
 import subprocess
 import sys
 import os
+import json
 
 ec2 = boto3.resource("ec2")
 s3 = boto3.resource("s3")
@@ -48,8 +49,8 @@ def create_instance():
     # create a public ip variable for instance
     pub_ip_inst = instance.public_ip_address
 
-   # ssh_check(instance)
-   # securecopy_check_webserver(pub_ip_inst)
+    ssh_check(instance)
+    securecopy_check_webserver(pub_ip_inst)
 
     return instance, pub_ip_inst
 
@@ -154,6 +155,24 @@ def create_bucket():
     try:
         # create bucket with location in Ireland
         response = s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'eu-west-1'})
+
+        bucket_policy = {
+            'version': '2012-10-17',
+            'Statement': [{
+                'Sid': 'AllowPublicRead',
+                'Effect': 'Allow',
+                'Principal': '*',
+                'Action': ['s3@GetObject'],
+                'Resource': 'arn:aws:s3:::' + bucket_name + '/*'
+            }]
+        }
+
+        bucket_policy = json.dumps(bucket_policy)
+        boto3.client('s3').put_bucket_policy(
+            Bucket=bucket_name,
+            Policy=bucket_policy
+        )
+
         # if bucket successfully created then print message for user
         print("creating bucket successful")
         print(response)
@@ -165,31 +184,42 @@ def create_bucket():
 
 
 # adding a file to a bucket
-def add_file_to_bucket(bucket_name, object_name):
-    # bucket_name = sys.argv[1]
-    # object_name = sys.argv[2]
+def add_file_to_bucket(bucket_name):
+    time.sleep(2)
+    print()
+    print("ADD AN IMAGE TO BUCKET")
+    print("-------------------")
+    time.sleep(1)
     try:
+        object_name = input("name of image: ")
         # command to add the file to the bucket                         # give file readable permission
         response = s3.Object(bucket_name, os.path.basename(object_name)).put(ACL='public-read', Body=open(object_name, 'rb'))
         print("File added to bucket called: ", bucket_name)
-        print(response)
+        print("Response: ", response)
     except Exception as error:
         print(error)
 
+    return object_name
+
 
 def add_file_to_index(instance, pub_ip_inst, bucket_name, object_name):
+    time.sleep(2)
+    print()
+    print("ADD IMAGE TO NGINX HOME PAGE")
+    print("-------------------")
+    time.sleep(1)
     try:
         # Ask user if they want to add the uploaded file to the index page for this instance
-        add_to_index = input("Add this file to index page (Y/N) ?: ")
+        add_to_index = input("Add this file to nginx home page (Y/N) ?: ")
         if add_to_index.upper() == 'Y':
             # Create a url for the uploaded file
             print("Creating URL for file")
-            index_url = "https://s3-eu-west-1.amazonaws.com/" + bucket_name + "/" + object_name
+            image_url = "https://s3-eu-west-1.amazonaws.com/" + bucket_name + "/" + object_name
 
             try:
                 # command to run to add the image to index.html using the url
-                cmd_add_image_to_index = "scp -i ~/dev-ops/paddykeypair.pem check_webserver.py ec2-user@" \
-                                + pub_ip_inst + "'echo \"<img src=" + index_url + ">\" >> /usr/share/nginx/html/index.html'"
+                cmd_add_image_to_index = "ssh -t -o StrictHostKeyChecking=no -i ~/dev-ops/paddykeypair.pem ec2-user@" \
+                                + pub_ip_inst + " 'sudo echo \"<img src=" + image_url + ">\" >> /usr/share/nginx/html/index.html'"
                 print("command to run: ", cmd_add_image_to_index)
                 # run the command and check the output & status
                 (status, output) = subprocess.getstatusoutput(cmd_add_image_to_index)
@@ -214,12 +244,12 @@ def add_file_to_index(instance, pub_ip_inst, bucket_name, object_name):
 
 def main():
     instance, pub_ip_inst = create_instance()
-    pub_ip_inst = ssh_check(instance)
-    securecopy_check_webserver(pub_ip_inst)
+   # pub_ip_inst = ssh_check(instance)
+    #securecopy_check_webserver(pub_ip_inst)
     execute_check_webserver(instance, pub_ip_inst)
-    (bucket_name, object_name) = create_bucket()
-   # add_file_to_bucket(bucket_name, object_name)
-    #add_file_to_index(instance, pub_ip_inst, bucket_name, object_name)
+    bucket_name = create_bucket()
+    object_name = add_file_to_bucket(bucket_name)
+    add_file_to_index(instance, pub_ip_inst, bucket_name, object_name)
 
 
 if __name__ == '__main__':
